f272826ca616efe1d38beec50bc49e75e647bd50
Van Muscari <vamuscari@gmail.com>
2021-04-12 13:44:39 -0500

    P bin test

--- Interpolation.jl
+++ Interpolation.jl
@@ -27,25 +27,44 @@ function main()
 
     b = 0
     f = 1
-    P(k, μ, pnum) = Inter(k) * (b + μ^2 * f)^2 * pnum(μ)
+    P(k, μ, pn) = Inter(k) * (b + μ^2 * f)^2 * pn(μ)
 
+    #hcubature(x -> P(x[1], x[2], p0), [0.1,-1],[0.2,1], abstol=1e-8)
 
+    #from 0 to 0.2 in steps of 0.01
+    
+    k = 0:0.01:0.2
+    
+    p0_bin = zeros(length(k) - 1)
+    println("\n","p0 bin")
+    for i in 2 :length(k) - 1
+        pval = hcubature(x -> P(x[1], x[2], p0), [k[i],-1],[k[i + 1],1], abstol=1e-8)
+        p0_bin[i] = pval[1] 
+        println("k: ", k[i] ," to ", k[i + 1] , "  p: ", pval)
+    end
 
-    #cubature test ------------------------------------------------------
-    func(x,y) = prod((x).^2 * y.^2)
-    println(hcubature(x -> func(x[1],x[2]), [-1,-1],[1,1], abstol=1e-8)) 
-    # 0.4444444444444444 is expectation. each int is 2/9. therefore, 4/9
-    #--------------------------------------------------------------------
+    p2_bin = zeros(length(k) - 1)
+    println("\n","p2 bin")
+    
+    for i in 2 :length(k) - 1
+        pval = hcubature(x -> P(x[1], x[2], p2), [k[i],-1],[k[i + 1],1], abstol=1e-8)
+        p2_bin[i] = pval[1] 
+        println("k: ", k[i] ," to ", k[i + 1] , "  p2: ", pval)
+    end
   
-    println(hcubature(x -> P(x[1], x[2], p0), [0.1,-1],[0.2,1], abstol=1e-8))
+    #hcubature(x -> P(x[1], x[2], p0), [0.1,-1],[0.2,1], abstol=1e-8)
 
     #Plotting----------------------------------------------
-    plot(camb[:,1], camb[:,2]) #Interpolation overlap
-    display(plot!(R, Inter(R)))
+    #plot(camb[:,1], camb[:,2]) #Interpolation overlap
+    #display(plot!(R, Inter(R)))
 
 
-    logR = log10.(LinRange( 0.0001,2.3,10000))
-    display(plot(logR, log10.(Inter(R))))
+    #logR = log10.(LinRange( 0.0001,2.3,10000))
+    #display(plot(logR, log10.(Inter(R))))
+    x = 1:20
+    display(plot(x, p0_bin))
+    display(plot(x, p2_bin))
+
     #-----------------------------------------------------
 
 
